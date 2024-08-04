bfdfec33e3f9e5f66ca95ca9b465fc100de15e0e
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-04 21:29:39 +0600

    add new program define variable

---
+++ variable.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var student1 string = "sohag" //type is string
+	var student2 = "saimum"       //type is inferred
+	x := 4                        //type is inferred with int variable
+	fmt.Println(student1)
+	fmt.Println(student2)
+	fmt.Println(x)
+
+}
