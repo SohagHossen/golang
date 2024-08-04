db045c5998fd9391343cad3e0636fe5d87caca59
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-04 21:51:53 +0600

    add new program define multivariable

---
+++ multivariable.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var a, b, c, d int = 2, 1, 4, 5
+	e, f := 3, "sohag"
+	var g bool
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+	fmt.Println(g)
+}
