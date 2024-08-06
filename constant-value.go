0cdbc32a169e919be332c8f24aa26a5724cc619c
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-06 23:15:42 +0600

    add new program constant value

---
+++ constant-value.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+// you diclear single or multiple constant
+// single constant like const PI =3.14
+const (
+	PI     = 3.14
+	A  int = 2
+)
+
+func main() {
+
+	fmt.Println(PI)
+	fmt.Println(A)
+
+}
