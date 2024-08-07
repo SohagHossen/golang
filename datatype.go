b21c0faa6a0fde8659c9a060dd8c88d680d76d10
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-07 22:43:52 +0600

    add a new program datatype

---
+++ datatype.go
@@ -0,0 +1,14 @@
+package main
+
+func main() {
+	var x int = 12
+	var y string = "sohag"
+	var z float32 = 3.14
+	var t bool = true
+
+	println("Integer value ", x)
+	println("String value ", y)
+	println("float32 value ", z)
+	println("Boolean value ", t)
+
+}
