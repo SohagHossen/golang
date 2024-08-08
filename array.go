76b48a7e7974887ac49f4e800d6703f2b9e945d3
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-08 23:05:00 +0600

    add new program is array

---
+++ array.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var arra1 = [4]int{1, 3, 4, 5}
+	var arr2 = [2]string{"sohag", "shuhanur"}
+	arr3 := [2]float32{12.3, 43.4}
+
+	fmt.Println(arra1)
+	fmt.Println(arr2)
+	fmt.Println(arr3)
+
+}
