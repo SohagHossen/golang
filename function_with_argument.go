40dec0f0969e1fa64d48a9d08281d025b034994a
SohagHossen <sohaghossen.cse@gmail.com>
2024-08-27 22:18:07 +0600

    function add with argument

---
+++ function_with_argument.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func newfunc(fname string) {
+
+	fmt.Println("Hi, I am ", fname, " from Bangladesh")
+
+}
+
+func main() {
+
+	newfunc("sohag")
+	newfunc("saimum")
+
+}
